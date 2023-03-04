e7f845447e9c95420d0c997eba9f1d856ad3de84
farhanbashir <farhan2526313@gmail.com>
2023-03-04 13:34:31 +0500

    4 march 2023

--- Q16.ts
+++ Q16.ts
@@ -1,1 +1,59 @@
+let myGuset3 = ["Ali","BooAli","Adeel","Liaqat Bhai","Afzal","Junaid"];
+
+console.log(`${myGuset3[0]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[1]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[2]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[3]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[4]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[5]}, I've invited you to dinner at my home on 8PM tonight. `);
+
+
+console.log(`Suddenly ${myGuset3[5]} is not coming tonight. So, i,ve replace them another friend. `);
+myGuset3[5] = "Ubaid Raza"
+
+console.log(`${myGuset3[0]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[1]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[2]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[3]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[4]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[5]}, I've invited you to dinner at my home on 8PM tonight. `);
+
+
+
+
+
+myGuset3.push("Hmaza","Tamoor","Faizan");
+console.log(`${myGuset3[0]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[1]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[2]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[3]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[4]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[5]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[6]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[7]}, I've invited you to dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[8]}, I've invited you to dinner at my home on 8PM tonight. `);
+
+console.log(`${myGuset3[8]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+console.log(`${myGuset3[7]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+console.log(`${myGuset3[6]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+console.log(`${myGuset3[5]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+console.log(`${myGuset3[4]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+console.log(`${myGuset3[3]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+console.log(`${myGuset3[2]}, Sorry! you can't invite them to dinner.. `);
+myGuset3.pop();
+
+console.log(`${myGuset3[0]}, You are still invited for dinner at my home on 8PM tonight. `);
+console.log(`${myGuset3[1]}, You are still invited for dinner at my home on 8PM tonight. `);
+myGuset3.pop();
+myGuset3.pop();
+
+console.log(myGuset3);
+
+
 
